c18d1271ef207a811dea5c1a12402de134b1c7a5
Blaine Bublitz <blaine@iceddev.com>
2015-08-01 22:02:40 -0700

    add sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
+  devtool: 'eval-source-map',
   entry: {
     bundle: [
       'webpack-dev-server/client?http://localhost:8080',
