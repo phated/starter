8d8e006830bbd69b7c4b95aee1adfd904b899b78
Blaine Bublitz <blaine@iceddev.com>
2015-08-12 15:38:09 -0700

    basic hot reload boilerplate

--- src/index.js
+++ src/index.js
@@ -13,3 +13,13 @@ const container = document.createElement('div');
 React.render(<Application store={store} />, container);
 
 document.body.appendChild(container);
+
+if(module.hot){
+  module.hot.accept('./reducers', function(){
+    const newReducers = require('./reducers');
+    const newStore = createStore(newReducers);
+    const replacementReducer = newStore.getReducer();
+    store.replaceReducer(replacementReducer);
+    React.render(<Application store={store} />, container);
+  });
+}
